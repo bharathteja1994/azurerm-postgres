01e83da0602d9e50b914cb0510bf954af1a6a638
bharathteja1994 <77113760+bharathteja1994@users.noreply.github.com>
2025-04-02 15:59:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,17 +35,40 @@ resource "azurerm_postgresql_server" "postgresql" {
   version                      = var.postgresql_version
   administrator_login          = var.administrator_login
   administrator_login_password = random_password.admin_password.result
+
   sku {
     name     = var.sku_name
     tier     = var.sku_tier
     capacity = var.sku_capacity
     family   = var.sku_family
   }
-  storage_mb = var.storage_mb
-  ssl_enforcement = var.ssl_enforcement
-  backup_retention_days = var.backup_retention_days
-  geo_redundant_backup = var.geo_redundant_backup
-  auto_grow = var.auto_grow
+
+  storage_mb                  = var.storage_mb
+  ssl_enforcement_enabled      = var.ssl_enforcement
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled  = var.geo_redundant_backup
+  auto_grow_enabled            = var.auto_grow
+  public_network_access_enabled = var.public_network_access
+  infrastructure_encryption_enabled = var.infrastructure_encryption
+  create_mode                  = var.create_mode
+  geo_redundant_backup_enabled  = var.geo_redundant_backup
+  identity {
+    type = "SystemAssigned"
+  }
+
+  threat_detection_policy {
+    enabled            = var.threat_detection_enabled
+    email_account_admins = var.email_account_admins
+    email_addresses    = var.email_addresses
+    retention_days     = var.threat_detection_retention_days
+  }
+
+  timeouts {
+    create = "60m"
+    update = "60m"
+    read   = "5m"
+    delete = "60m"
+  }
 }
 
 resource "azurerm_postgresql_database" "database" {
@@ -54,6 +77,12 @@ resource "azurerm_postgresql_database" "database" {
   server_name         = azurerm_postgresql_server.postgresql.name
   charset             = var.charset
   collation           = var.collation
+
+  timeouts {
+    create = "60m"
+    read   = "5m"
+    delete = "60m"
+  }
 }
 
 data "azurerm_client_config" "current" {}
