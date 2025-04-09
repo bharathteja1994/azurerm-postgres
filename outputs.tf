3401629b20e6f0cc4d364d643faebc28c8da6927
bharathteja1994 <77113760+bharathteja1994@users.noreply.github.com>
2025-04-10 01:22:03 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,14 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "postgresql_flexible_server_id" {
+  description = "The ID of the PostgreSQL Flexible Server"
+  value       = azurerm_postgresql_flexible_server.example.id
 }
 
-output "postgresql_server_name" {
-  value = azurerm_postgresql_server.postgresql.name
+output "user_assigned_identity_id" {
+  description = "The ID of the user-assigned identity"
+  value       = azurerm_user_assigned_identity.example.id
 }
 
-output "database_name" {
-  value = azurerm_postgresql_database.database.name
-}
-
-output "administrator_login" {
-  value = azurerm_postgresql_server.postgresql.administrator_login
-}
-
-output "key_vault_uri" {
-  value = azurerm_key_vault.kv.vault_uri
-}
-
-output "admin_password" {
-  value     = random_password.admin_password.result
-  sensitive = true
+output "private_endpoint_id" {
+  description = "The ID of the private endpoint"
+  value       = azurerm_private_endpoint.example.id
 }
