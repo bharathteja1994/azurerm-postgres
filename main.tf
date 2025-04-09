3ad6eec8afad8d69f89bd5af9fa39e5b99f401e4
bharathteja1994 <77113760+bharathteja1994@users.noreply.github.com>
2025-04-10 01:20:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,88 +1,71 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
+  tags     = var.tags
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                = "${var.resource_group_name}-kv"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "standard"
-  tenant_id          = data.azurerm_client_config.current.tenant_id
-}
+resource "azurerm_postgresql_flexible_server" "example" {
+  name                = var.server_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  administrator_login = var.admin_username
+  administrator_password = var.admin_password
+  version             = "13"
+  sku_name            = "Standard_B1ms"
+  storage_mb          = 32768
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+  tags     = var.tags
 
-resource "random_password" "admin_password" {
-  length  = 16
-  special = true
-}
-
-resource "azurerm_key_vault_secret" "admin_login" {
-  name         = "postgres-admin-login"
-  value        = var.administrator_login
-  key_vault_id = azurerm_key_vault.kv.id
-}
-
-resource "azurerm_key_vault_secret" "admin_password_secret" {
-  name         = "postgres-admin-password"
-  value        = random_password.admin_password.result
-  key_vault_id = azurerm_key_vault.kv.id
-}
-
-resource "azurerm_postgresql_server" "postgresql" {
-  name                         = var.postgresql_server_name
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = var.postgresql_version
-  administrator_login          = var.administrator_login
-  administrator_login_password = random_password.admin_password.result
-
-  sku {
-    name     = var.sku_name
-    tier     = var.sku_tier
-    capacity = var.sku_capacity
-    family   = var.sku_family
-  }
-
-  storage_mb                  = var.storage_mb
-  ssl_enforcement_enabled      = var.ssl_enforcement
-  backup_retention_days        = var.backup_retention_days
-  geo_redundant_backup_enabled  = var.geo_redundant_backup
-  auto_grow_enabled            = var.auto_grow
-  public_network_access_enabled = var.public_network_access
-  infrastructure_encryption_enabled = var.infrastructure_encryption
-  create_mode                  = var.create_mode
-  geo_redundant_backup_enabled  = var.geo_redundant_backup
   identity {
-    type = "SystemAssigned"
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.example.id]
   }
 
-  threat_detection_policy {
-    enabled            = var.threat_detection_enabled
-    email_account_admins = var.email_account_admins
-    email_addresses    = var.email_addresses
-    retention_days     = var.threat_detection_retention_days
+  authentication {
+    active_directory_auth_enabled = true
   }
 
-  timeouts {
-    create = "60m"
-    update = "60m"
-    read   = "5m"
-    delete = "60m"
+  private_dns_zone_id = azurerm_private_dns_zone.example.id
+
+  data_encryption {
+    type = "UserAssigned"
+    key_vault_key_id = var.key_vault_key_id
   }
 }
 
-resource "azurerm_postgresql_database" "database" {
-  name                = var.database_name
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_postgresql_server.postgresql.name
-  charset             = var.charset
-  collation           = var.collation
+resource "azurerm_user_assigned_identity" "example" {
+  name                = var.identity_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  tags     = var.tags
+}
 
-  timeouts {
-    create = "60m"
-    read   = "5m"
-    delete = "60m"
+resource "azurerm_private_endpoint" "example" {
+  name                = var.private_endpoint_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  subnet_id           = var.subnet_id
+  tags     = var.tags
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_postgresql_flexible_server.example.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
   }
 }
 
-data "azurerm_client_config" "current" {}
+resource "azurerm_private_dns_zone" "example" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.example.name
+  tags     = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "example-link"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = var.virtual_network_id
+  tags     = var.tags
+}
